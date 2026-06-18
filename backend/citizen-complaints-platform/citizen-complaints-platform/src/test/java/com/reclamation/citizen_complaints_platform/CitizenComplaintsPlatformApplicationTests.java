42f520f47f3d891b7ad75b77f5563392ee06d991
Boube24 <24012@SupNum.mr>
2026-06-18 12:33:27 +0000

    projet spring boot

---
+++ backend/citizen-complaints-platform/citizen-complaints-platform/src/test/java/com/reclamation/citizen_complaints_platform/CitizenComplaintsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reclamation.citizen_complaints_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenComplaintsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
